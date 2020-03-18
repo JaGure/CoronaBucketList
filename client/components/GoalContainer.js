95c97bee650a4ea2323db1a8f9af52c4c47b28c6
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-03-18 18:48:40 -0400

    Added GoalList rendering to GoalContainer

--- client/components/GoalContainer.js
+++ client/components/GoalContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import s from 'styled-components'
+import GoalList from './GoalList'
 
 const Wrapper = s.div`
     background-color: #f0efeb;
@@ -11,10 +12,46 @@ const Wrapper = s.div`
     }
 `
 
+// wraps the body of the container (its only children will be rows)
+const BodyWrapper = s.div`
+    display: flex;
+    flex-flow: column nowrap;
+`
+
+// child of the body wrapper. holds the GoalLists
+const GoalListRow = s.div`
+    flex: initial;
+    display: flex;
+    flex-flow: row nowrap;
+`
+
+const GoalListWrapperCSS = s.div`
+    flex: 1 1 auto;
+`
+// each wrapper holds a goal list - allows for different sized list blocks
+const GoalListWrapper = () => {
+    return (
+        <GoalListWrapperCSS>
+            <GoalList />
+        </GoalListWrapperCSS>
+    )
+}
+
+
+
 const GoalContainer = () => {
     return (
         <Wrapper>
             <h2>Goals</h2>
+            <BodyWrapper>
+                <GoalListRow>
+                    <GoalListWrapper />
+                    <GoalListWrapper />
+                </GoalListRow>
+                <GoalListRow>
+                    <GoalListWrapper />
+                </GoalListRow>
+            </BodyWrapper>
         </Wrapper>
     )
 }
