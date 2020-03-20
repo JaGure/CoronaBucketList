bad212792eef1cb786eb45fbb8fa5d772315ecee
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-03-20 16:06:24 -0400

    Added get bucket list request to server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const path = require('path')
+const bucketListParser = require('./parsers/bucketListParser')
 
 const app  = express()
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (_, res) => {
-    console.log("ASDSFASDF")
-    res.send("👀")
+app.get('/bucket-list', (_, res) => {
+  bucketListParser.getBucketList(data => {
+    res.send(data)
+  })
 })
 
+// starting server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
