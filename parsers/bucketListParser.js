32f29069b5aea81430ea13c974f78246dcdbe224
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-03-20 15:59:40 -0400

    Added get bucket list request to server

--- parsers/bucketListParser.js
+++ parsers/bucketListParser.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 // async reads the bucketlist, passes it to the next function
 const getBucketList = next => {
-    fs.readFile('../data/bucketList.txt', 'utf-8', function(err, data){
+    fs.readFile('./data/bucketList.txt', 'utf-8', function(err, data){
         if (err) throw err
 
         const bucketList = JSON.parse(data).bucketList
@@ -11,14 +11,19 @@ const getBucketList = next => {
     });
 }
 
-// const updateBucketList = data => {
-//     // let newData = data.replace(/\n/g, '')
-//     // fs.writeFile('bucketList.txt', newData, 'utf-8', function (err) {
-//     //     if (err) throw err;
-//     //     console.log('filelistAsync complete');
-//     //   });
-//             // fs.writeFile('filelistAsync.txt', newValue, 'utf-8', function (err) {
-//         //   if (err) throw err;
-//         //   console.log('filelistAsync complete');
-//         // });
-// }
+const updateBucketList = data => {
+    // let newData = data.replace(/\n/g, '')
+    // fs.writeFile('bucketList.txt', newData, 'utf-8', function (err) {
+    //     if (err) throw err;
+    //     console.log('filelistAsync complete');
+    //   });
+            // fs.writeFile('filelistAsync.txt', newValue, 'utf-8', function (err) {
+        //   if (err) throw err;
+        //   console.log('filelistAsync complete');
+        // });
+}
+
+module.exports = {
+    getBucketList: getBucketList,
+    updateBucketList: updateBucketList
+}
