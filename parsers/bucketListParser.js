b91fe7f96d822c846e8251174b428b8b7de13724
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-03-21 17:16:58 -0400

    Added add to bucket list functions to bucketListParser

--- parsers/bucketListParser.js
+++ parsers/bucketListParser.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 
+const bucketListFile = './data/bucketList.json'
+
 // async reads the bucketlist, passes it to the next function
 const getBucketList = next => {
-    fs.readFile('./data/bucketList.txt', 'utf-8', function(err, data){
+    fs.readFile(bucketListFile, 'utf-8', function(err, data){
         if (err) throw err
 
         const bucketList = JSON.parse(data).bucketList
@@ -11,19 +13,74 @@ const getBucketList = next => {
     });
 }
 
-const updateBucketList = data => {
-    // let newData = data.replace(/\n/g, '')
-    // fs.writeFile('bucketList.txt', newData, 'utf-8', function (err) {
-    //     if (err) throw err;
-    //     console.log('filelistAsync complete');
-    //   });
-            // fs.writeFile('filelistAsync.txt', newValue, 'utf-8', function (err) {
-        //   if (err) throw err;
-        //   console.log('filelistAsync complete');
-        // });
+// writes the passed in bucketlist to the file
+const updateBucketList = updatedBucketList => {
+    const bucketListJSON = JSON.stringify({ bucketList: updatedBucketList })
+
+    fs.writeFile(bucketListFile, bucketListJSON, 'utf-8', function (err) {
+        if (err) throw err;
+        console.log('Bucket List successfully updated!')
+    })
+}
+
+// finds where to and then inserts a new goal into the bucket list
+const addGoalToBucketList = (goalName, location) => {
+    getBucketList(bucketList => {
+        if (!Array.isArray(location)) {
+            console.log('Location Not An Array!')
+            return
+        } else if (location.length < 1) {
+            console.log('Invalid Location Array!')
+            return
+        }
+
+        // find where to insert (insertArray will always be a children array)
+        let insertArray = bucketList[location[0]].children
+
+        for (let i = 1; i < location.length; i++) {
+            insertArray = insertArray[location[i]].children
+        }
+
+        // check if goal already exists in insertArray
+        for (let i = 0; i < insertArray.length; i++) {
+            if (insertArray[i].goalName === goalName) {
+                console.log('Goal Already Exists At This Location!')
+                return
+            }
+        }
+
+        const newGoal = {
+            goalName: goalName,
+            children: []
+        }
+
+        insertArray.push(newGoal)
+        updateBucketList(bucketList)
+    })
+}
+
+// appends a new goal group to the end of the bucket list
+const addNewGoalGroupToBucketList = (goalGroupName) => {
+    getBucketList(bucketList => {
+        for (let i = 0; i < bucketList.length; i++) {
+            if (bucketList[i].goalGroupName === goalGroupName) {
+                console.log('Attempted to add already existing goalGroup!')
+                return
+            }
+        }
+
+        const newGoalGroup = {
+            goalGroupName: goalGroupName,
+            children: []
+        }
+
+        bucketList.push(newGoalGroup)
+        updateBucketList(bucketList)
+    })
 }
 
 module.exports = {
     getBucketList: getBucketList,
-    updateBucketList: updateBucketList
+    addGoalToBucketList: addGoalToBucketList,
+    addNewGoalGroupToBucketList: addNewGoalGroupToBucketList
 }
