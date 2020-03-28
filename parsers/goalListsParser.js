7458f8884a333c86dfcdb8d871ed4f27952e5a4c
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-03-28 15:05:03 -0400

    Added write functionality to goal list parser

--- parsers/goalListsParser.js
+++ parsers/goalListsParser.js
@@ -13,6 +13,41 @@ const getGoalLists = next => {
     });
 }
 
+// async updates the goal list of the given list owner by adding the given goal
+const updateGoalLists = function (listOwner, goalToAdd) {
+    getGoalLists(goalLists => {
+        if (typeof(goalToAdd) !== 'string') {
+            console.log("Invalid New Goal Type")
+        }
+
+        let ownerList = []
+        let ownerIndex = -1
+
+        for (let i = 0; i < goalLists.length; i++) {
+            if (goalLists[i].listOwner === listOwner) {
+                ownerList = goalLists[i].elements
+                ownerIndex = i
+            }
+        }
+
+        if (ownerIndex === -1) {
+            console.log("Invalid List Owner")
+            return
+        }
+
+        ownerList.push(goalToAdd)
+        goalLists[ownerIndex].elements = ownerList
+
+        const goalListsJSON = JSON.stringify({ goals: goalLists })
+
+        fs.writeFile(goalListsFile, goalListsJSON, 'utf-8', function (err) {
+            if (err) throw err;
+            console.log('Goal Lists successfully updated!')
+        })
+    })
+}
+
 module.exports = {
-    getGoalLists: getGoalLists
+    getGoalLists: getGoalLists,
+    updateGoalLists: updateGoalLists
 }
