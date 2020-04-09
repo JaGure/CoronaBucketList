1f0ed93ad6a9324bd5656b72db670036cfc13a83
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-04-09 03:50:13 -0400

    Reverted the testing stuff I did - don't  need it

--- client/index.js
+++ client/index.js
@@ -4,13 +4,3 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-console.log("tiddies" + document.location.origin)
-
-const request = new Request('/.netlify/functions/server/goal-lists', {
-    method: 'POST', 
-    body: JSON.stringify({listOwner: 'Jake', goalToAdd: 'did this work?'}),
-    headers: { "Content-Type": "application/json" }
-})
-
-fetch(request)
