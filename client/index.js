f66a2570d311a63b869ab055dc6767d778e6bbaf
Jake Gurevitch <jake.gurevitch@gmail.com>
2020-04-09 03:47:59 -0400

    Testing getting own URL - need to push to put on site

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(<App />, document.getElementById('root'))
 
 console.log("tiddies" + document.location.origin)
 
-const request = new Request('https://5e8ed073eb4e8c0007b9241a--nostalgic-pasteur-1adf7e.netlify.com/.netlify/functions/server/goal-lists', {
+const request = new Request('/.netlify/functions/server/goal-lists', {
     method: 'POST', 
     body: JSON.stringify({listOwner: 'Jake', goalToAdd: 'did this work?'}),
     headers: { "Content-Type": "application/json" }
